22c56d13cf62df66357708565767177270c3fee1
seijuro <seijuro78@gmail.com>
2017-08-09 23:18:23 +0900

    snapshot

---
+++ src/main/java/com/github/seijuro/task/specific/BusinessPlaceTable.java
@@ -0,0 +1,9 @@
+package com.github.seijuro.task.specific;
+
+public class BusinessPlaceTable {
+    public static final String TablName = "BusinessPlace";
+
+    public static class Columns {
+        public static final String SequenceId = "seq";
+    }
+}
